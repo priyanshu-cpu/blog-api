8320b9bd3e1162042290364c51443260450b55b8
priyanshu-cpu <priyanshurajput12863@gmail.com>
2026-09-12 07:58:49 +0530

    get blog by Id

--- main.py
+++ main.py
@@ -25,7 +25,17 @@ def create_blog(blog_data: schemas.BlogBase, db: Session = Depends(get_db)):
     db.refresh(data)
     return data
 
+
 @app.get("/blogs", response_model=list[schemas.BlogOut])
 def get_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.BlogData).all()
     return blogs
+
+
+@app.get("/blogs/{blog_id}", response_model=schemas.BlogOut)
+def get_blog(blog_id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.BlogData).filter(models.BlogData.id == blog_id).first()
+    if blog is None:
+        raise HTTPException(status_code=401, detail="Blog not found!")
+
+    return blog
